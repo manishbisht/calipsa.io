b713b4768055055a18fa2b8daf9691f421ca068d
Manish Bisht <manish.bisht490@gmail.com>
2019-04-02 23:46:57 +0530

    project docs added

--- Bloom.js
+++ Bloom.js
@@ -2,13 +2,16 @@ var mmh3 = require('murmurhash3');
 
 class Bloom {
     constructor() {
+        // Initialize an array length 2 ^ 32 with empty values
         this.bitArray = new Array(4294967295);
     }
 
+    // Convert Text to 32 Bit Integer (in Decimal)
     convertTextToDecimalNumber(text) {
         return mmh3.murmur32Sync(text);
     }
 
+    // Convert 32 Bit Integer (in Decimal) to Binary String
     convertDecimalNumberToBitString(decimalNumber) {
         let stringArray = [];
         let i = 0;
@@ -20,6 +23,7 @@ class Bloom {
         return stringArray.join("");
     }
 
+    // Add the string to the filter
     add(text) {
         let decimalNumber = this.convertTextToDecimalNumber(text);
         let bitString = this.convertDecimalNumberToBitString(decimalNumber);
@@ -30,6 +34,7 @@ class Bloom {
         }
     }
 
+    // Check if value is present
     check(text) {
         let isPresent = true;
         let decimalNumber = this.convertTextToDecimalNumber(text);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# calipsa.io
+# calipsa.io - Bloom Filter
+
+# How to Run and Test the project
+1. Clone/Download the project repo.
+2. Go to the project directory.
+3. Install Dependencies `npm install`
+4. Test project `nom test`
+
+# References
+- https://en.wikipedia.org/wiki/Bloom_filter
+- https://www.jasondavies.com/bloomfilter/
+- https://www.geeksforgeeks.org/bloom-filters-introduction-and-python-implementation/
+- https://www.npmjs.com/package/murmurhash3
